e0347b66fce2e626bb80e767b3a08ffc35be19f2
triniz <triki.nizar@gmail.com>
2019-02-17 16:42:39 -0500

    Add admin pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,12 @@ class StaticPagesController < ApplicationController
 
   def product
   end
+  def login
+  end
+  def signup
+  end
+  def form
+  end
+  def dashboard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get 'static_pages/contact'
   get 'static_pages/products'
   get 'static_pages/product'
+  get 'static_pages/login'
+  get 'static_pages/signup'
+  get 'static_pages/form'
+  get 'static_pages/dashboard'
+  get 'static_pages/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'static_pages#index'
 end
