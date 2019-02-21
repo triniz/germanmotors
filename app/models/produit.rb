23189e701d9014bca51787a85841a73c18c0e621
triniz <triki.nizar@gmail.com>
2019-02-21 14:16:20 -0500

    Add dropdown in rails_admin

--- app/models/produit.rb
+++ app/models/produit.rb
@@ -1,6 +1,16 @@
 class Produit < ApplicationRecord
 	has_paper_trail
 	mount_uploader :image, ImageUploader
+
 	has_many :compatibilites
   	has_many :modeles, through: :compatibilites
+
+  	def type_produit_enum
+	    # Do not select any value, or add any blank field. RailsAdmin will do it for you.
+	    ['Moteur', 'Tableau de bord','Pare-brise','Phares','Suspension', "kit d'embrayage"]
+  	end
+  	def etat_enum
+	    # Do not select any value, or add any blank field. RailsAdmin will do it for you.
+	    ['Neuf', 'Occasion']
+  	end
 end
