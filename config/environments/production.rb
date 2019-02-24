3b7a34d591dd047f36aef6c05370df8bd8846a4c
triniz <triki.nizar@gmail.com>
2019-02-24 11:16:01 -0500

    Fix assets issue on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
+  #fix for application.css asset not found in production mode on Heroku
+  config.serve_static_assets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
